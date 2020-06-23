3f6bb4abf2d37ec6b05b724e0a35ee2fbe1aa559
Naveen Kumar <naveenkumar.murugan@hidglobal.com>
2020-06-23 09:41:30 +0530

    removed api keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyCicNlnQLmK46xksWHhxlDmIiE8qYIrGe4",
-    authDomain: "inventbill.firebaseapp.com",
-    databaseURL: "https://inventbill.firebaseio.com",
-    projectId: "inventbill",
-    storageBucket: "inventbill.appspot.com",
-    messagingSenderId: "286529859632",
-    appId: "1:286529859632:web:01a3cc07eba881751b3df5",
-    measurementId: "G-5DPZJG9J5B"
-  }
 };
